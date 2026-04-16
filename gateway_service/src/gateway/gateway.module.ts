1fed5de5dbe8e77adcae615a71fbf073e41ea4f1
HHuongDuong <okakoro1907@gmail.com>
2026-04-16 18:23:51 +0700

    internal ws 4 role assign

--- gateway_service/src/gateway/gateway.module.ts
+++ gateway_service/src/gateway/gateway.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RoomGateway } from './room.gateway';
 import { RoomServiceClient } from './room.client';
 import { KafkaConsumerService } from './kafka.consumer';
+import { InternalWsController } from './internal-ws.controller';
 
 @Module({
   providers: [RoomGateway, RoomServiceClient, KafkaConsumerService],
+  controllers: [InternalWsController],
 })
 export class GatewayModule {}
